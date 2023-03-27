607705dfafcfe8dcf778e13c766f3d8e57680360
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-03-27 08:23:47 +0400

    add another util Type

--- packages/ts-ghost-core-api/src/utils.d.ts
+++ packages/ts-ghost-core-api/src/utils.d.ts
@@ -3,3 +3,6 @@ export declare type Mask<Obj> = {
 };
 
 export declare type InferGhostResponseData<T> = T extends { status: "success"; data: infer D } ? D : never;
+export declare type InferFetcherData<T extends { fetch: () => Promise<any> }> = InferGhostResponseData<
+  Awaited<ReturnType<T["fetch"]>>
+>;
