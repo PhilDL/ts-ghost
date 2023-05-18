d510484f9b3b7c53a55a01366815bb1ddf23c404
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-05-18 21:52:07 +0400

    cleanup nextjs example

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -1,8 +1,8 @@
 import { ghostAdminAPI, ghostContentAPI } from "./ghost";
 
 async function getBlogPosts() {
-  const response = await ghostAdminAPI.posts.browse().fields({title: true, slug:true, id:true}).fetch();
-  if(!response.success) {
+  const response = await ghostAdminAPI.posts.browse().fields({ title: true, slug: true, id: true }).fetch();
+  if (!response.success) {
     throw new Error(response.errors.join(", "));
   }
   return response.data;
@@ -10,19 +10,24 @@ async function getBlogPosts() {
 
 async function getSiteSettings() {
   const response = await ghostContentAPI.settings.fetch();
-  if(!response.success) {
+  if (!response.success) {
     throw new Error(response.errors.join(", "));
   }
   return response.data;
 }
 
 export default async function HomePage() {
-  const [posts, settings] = await Promise.all([getBlogPosts(), getSiteSettings()])
-  return <div>
-    <h1>This is a list of posts for {settings.title}:</h1>
-    <ul>
-      {(posts).map(post => <li key={post.id}>{post.title} ({post.slug})</li>)}
-    </ul>
-    
-  </div>;
+  const [posts, settings] = await Promise.all([getBlogPosts(), getSiteSettings()]);
+  return (
+    <div>
+      <h1>This is a list of posts for {settings.title}:</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.title} ({post.slug})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
