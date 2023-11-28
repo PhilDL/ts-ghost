9292a40cbafe30d229a06dfd6bd08393aba18a36
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-11-28 13:36:13 +0400

    Add back optional "html" field to adminPostsSchema

--- packages/ts-ghost-admin-api/src/schemas/posts.ts
+++ packages/ts-ghost-admin-api/src/schemas/posts.ts
@@ -30,6 +30,7 @@ export const adminPostsSchema = basePostsSchema.merge(
         '{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}',
       )
       .optional(),
+    html: z.string().catch("").optional(),
     plaintext: z.string().catch("").optional(),
   }),
 );
